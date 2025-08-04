9a042fd88721384fee827f1813e4b1284fdeaaf4
Hayder Atiyah <eng.hayderAtiyah@gmail.com>
2025-08-04 10:27:08 -0400

    first class

--- backend/app.py
+++ backend/app.py
@@ -5,5 +5,26 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/api/message")
+def data():
+    return jsonify({"message": "HEY THIS IS THE FIRST CLASS"})
+
+
+@app.route("/api/age")
+def getAge():
+    return jsonify({"age": 20})
+
+
+@app.route("/api/user")
+def user_info():
+    user_data = {
+        "name": "Mayur",
+        "age": 50,
+        "skills": ["Python", "Flask", "React"],
+        "profile": {"github": "", "location": "USA"},
+    }
+    return jsonify(user_data)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,37 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("hello");
+  const [age, setAge] = useState(90);
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  });
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/age")
+      .then((res) => res.json())
+      .then((data) => setAge(data.age));
+  });
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/user")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
 
   return (
     <div className="App">
       <h1>Flask + React</h1>
       <p>{message}</p>
+      <p>{age} years old</p>
+      <p>name: {user.name}</p>
+      <p>age: {user.age}</p>
+      <p>skills: {user.skills.join(", ")}</p>
     </div>
   );
 }
