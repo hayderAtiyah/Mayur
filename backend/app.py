eadb40bcef0c3bf5ae1668e1f3a0a816ce90d3c1
4wqrs12 <forzstreet61@gmail.com>
2025-08-15 10:13:00 -0400

    class

--- backend/app.py
+++ backend/app.py
@@ -33,6 +33,30 @@ def list_names():
     return jsonify({"success": True, "data": docs})
 
 
+@app.route("/api/delete-name", methods=["POST"])
+def delete_name():
+    try:
+        data = request.get_json()
+        name = data.get("name")
+        if not name:
+            return jsonify({"success": False, "error": "No name provided"}), 400
+        names_col.delete_one({"name": name})
+        return jsonify({"success": True, "message": "Name removed"})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
+
+@app.route("/api/search-name", methods=["POST"])
+def search_name():
+    try:
+        data = request.get_json()
+        name = data.get("name")
+        names_col.find_one({"names": name})
+        return jsonify({"success": True, "message": "Name found"})
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
+  const [searchName, setSearchName] = useState("");
+  const [result, setResult] = useState("");
   const [rows, setRows] = useState([]);
   const [error, setError] = useState("");
 
@@ -16,9 +18,73 @@ function App() {
     fetchNames();
   }, []);
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://127.0.0.1:5000/api/add-name", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name})
+      });
+      if (!res.ok) {
+        throw new Error("Server error "+res.status)
+      }
+      fetchNames();
+      setName("");
+    } catch(err) {
+      console.log(`The error: ${err.message}`)
+    }
+  }
+
+  async function handleDelete(deletedName) {
+    try {
+      const res = await fetch("http://127.0.0.1:5000/api/delete-name", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name: deletedName})
+      });
+      if (!res.ok) {
+        throw new Error("Server error "+res.status)
+      }
+      fetchNames();
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
+  }
+
+  async function handleSearch(e, nameToSearch) {
+    e.preventDefault();
+    try {
+      const res = await fetch("http://127.0.0.1:5000/api/search-name", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name: nameToSearch})
+      })
+      if (!res.ok) {
+        throw new Error("Server error "+res.status)
+      }
+      const json = await res.json();
+      if (json.success && json.data) {
+        setResult("Name found");
+
+      } else {
+        setResult("Name not found");
+      }
+      setSearchName("");
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <p>Enter your name: </p>
         <input
           type="text"
@@ -30,6 +96,12 @@ function App() {
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
 
+      <div className="search">
+        <input type="text" placeholder="Enter name" value={searchName} onChange={(e) => setSearchName(e.target.value)} />
+        <button onClick={(e) => handleSearch(e, searchName)}>Search</button>
+      </div>
+      {result !== "" && <p>{result}</p>}
+      
       <table border="1" cellPadding="6" style={{ marginTop: "20px" }}>
         <thead>
           <tr>
@@ -47,7 +119,7 @@ function App() {
               <td>{new Date(r.createdAt).toLocaleString()}</td>
               <td>
                 {/* Button to delete this name */}
-                {/* <button onClick={() => handleDelete(r.name)}>Delete</button> */}
+                <button onClick={() => handleDelete(r.name)}>Delete</button>
               </td>
             </tr>
           ))}
